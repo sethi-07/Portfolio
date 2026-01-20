cc922c4f72d6ee7d8bb2be78353897fcfda46e22
Abhinandan Sethi <abhinandansethi07@gmail.com>
2026-01-21 01:01:50 +0530

    Theme toggle added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
 
 const Home = () => {
   return (
-    <div className='min-h-srceen '>
+    <div className='min-h-srceen bg-background text-foreground overflow-x-hidden'>
       {/* Theme toggle */}
+      <ThemeToggle/>
       {/* Meteor Shower */}
 
       {/* Navbar */}
