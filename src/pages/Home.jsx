9e38883e946ddd82ccc35acfedd00bc41bec2e7e
Abhinandan Sethi <abhinandansethi07@gmail.com>
2026-01-23 01:20:56 +0530

    Hero section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ThemeToggle from '../components/ThemeToggle'
 import StarBackground from '../components/StarBackground'
 import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
@@ -13,6 +14,9 @@ const Home = () => {
       {/* Navbar */}
       <Navbar/>
       {/* Main */}
+      <main>
+        <HeroSection/>
+      </main>
       {/* Footer */}
     </div>
   )
