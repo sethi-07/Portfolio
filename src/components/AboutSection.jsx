a60594aff3484c16abe230c5c9163b4deeddcf97
Abhinandan Sethi <abhinandansethi07@gmail.com>
2026-01-31 02:35:13 +0530

    About Section additions

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,12 +1,69 @@
+import { Briefcase, Code, User } from 'lucide-react'
 import React from 'react'
 
 const AboutSection = () => {
   return (
     <section id = "about" className='py-24 px-4 relative'>
       <div className='container mx-auto max-w-5xl'>
-        <h2 className='text-3xl md-text-4xl font-bold mb-12 text-center'>
+        <h2 className='text-3xl md:text-4xl font-bold mb-12 text-center'>
           About <span className='text-primary'> Me </span>
         </h2>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-12 items-center'>
+          <div className='space-y-6'>
+            <h3 className='text-2xl font-semibold'>Passionate Web Developer & Tech Creator</h3>
+            <p className='text-foreground'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia accusamus voluptate voluptas nulla culpa magni dolorem, reiciendis suscipit at ab. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </p>
+
+            <p className='text-foreground'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia accusamus voluptate voluptas nulla culpa magni dolorem, reiciendis suscipit at ab. 
+            </p>
+
+            <div className='flex flex-col sm:flex-row gap-4 pt-4 justify-center'>
+              <a href ="#contact" className='cosmic-button'>Get In Touch</a>
+              <a href = "" className='px-6 py-2 rounded-full border border-primary text-primary'>
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className='grid grid-cols-1 gap-6'>
+            <div className='gradient-border p-6 card-hover'>
+              <div className='flex items-start gap-4'>
+                <div className='p-3 rounded-full bg-primary/10'>
+                  <Code className='h-6 w-6 text-primary '/>
+                </div>
+                <div className='text-left'>
+                    <h4 className='font-semibold text-foreground'>Web Development</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, facere.</p>
+                </div>
+              </div>
+            </div>
+            <div className='gradient-border p-6 card-hover'>
+              <div className='flex items-start gap-4'>
+                <div className='p-3 rounded-full bg-primary/10'>
+                  <User className='h-6 w-6 text-primary '/>
+                </div>
+                <div className='text-left'>
+                    <h4 className='font-semibold text-foreground'>UI/UX Design</h4>
+                    <p>Designing intuitive user interfaces and seamless user experiences.</p>
+                </div>
+              </div>
+            </div>
+            <div className='gradient-border p-6 card-hover'>
+              <div className='flex items-start gap-4'>
+                <div className='p-3 rounded-full bg-primary/10'>
+                  <Briefcase className='h-6 w-6 text-primary '/>
+                </div>
+                <div className='text-left'>
+                    <h4 className='font-semibold text-foreground'>Data Analysis</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, facere.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
     </section>
